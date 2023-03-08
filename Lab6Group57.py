bfc2e24c3c1131b4262f91b0c9c436c3be161721
mjengert <m.engert@ufl.edu>
2023-03-07 18:26:30 -0600

    decode function complete

--- Lab6Group57.py
+++ Lab6Group57.py
@@ -5,24 +5,40 @@ def encoder(password):
     enpass = ""
     #shifts password upwards by 3 integers#
     for i in new_list:
-        if i == "10":
+        if i == 10:
             i = 0
-        if i == "11":
+        if i == 11:
             i = 1
-        if i == "12":
+        if i == 12:
             i = 2
         enpass += str(i) + ""
     print("Your password has been encoded and stored! ")
     return enpass
     #returns enpass to be later used#
 def decoder(enpass):
-    pass
+    enc_password = []
+    for num in enpass:
+        enc_password.append(int(num))
+    dec_password = []
+    for value in enc_password:
+        value -= 3
+        if value == -1:
+            value = 9
+        if value == -2:
+            value = 0
+        if value == -3:
+            value == 1
+        dec_password.append(str(value))
+    password_dec = ''.join(dec_password)
+    print(f'The encoded password is {enpass}, and the original password is {password_dec}')
+    return password_dec
+
 if __name__ == "__main__":
     while True:
         print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit")
         menu = input("Please enter an option: ")
         if menu == "1":
-            password = int(input("Please enter your password to encode: "))
+            password = input("Please enter your password to encode: ")
             encoder(password)
         elif menu == "2":
             enpass = encoder(password)
